f6258387bb1e147908bdf0c05febb1b285d74a51
Aman Kumar Ayush <amankumar.ayush@thoughtworks.com>
2022-04-12 20:31:44 +0530

    BAH-1503 | Add. create an Upload reports button to navigate to report upload screen
    co-authored by Priyanka<pajmera@thoughtworks.com>

--- src/patient-lab-details/patient-lab-details.tsx
+++ src/patient-lab-details/patient-lab-details.tsx
@@ -1,16 +1,17 @@
 import {ExtensionSlot, usePatient} from '@openmrs/esm-framework'
+import {Button} from 'carbon-components-react'
 import React from 'react'
 import {Link, RouteComponentProps} from 'react-router-dom'
 import Loader from '../loader/loader.component'
 import styles from './patient-lab-details.scss'
 
-interface PatientLabChartParams {
+interface PatientParamsType {
   patientUuid: string
 }
 
-const PatientLabDetails: React.FC<RouteComponentProps<
-  PatientLabChartParams
->> = ({match}) => {
+const PatientLabDetails: React.FC<RouteComponentProps<PatientParamsType>> = ({
+  match,
+}) => {
   const {patientUuid} = match.params
   const {isLoading, patient, error} = usePatient(patientUuid)
   return (
@@ -21,12 +22,24 @@ const PatientLabDetails: React.FC<RouteComponentProps<
         <div>Something went wrong: {error.message}</div>
       ) : (
         <div>
-          <aside>
-            <ExtensionSlot
-              extensionSlotName="patient-header-slot"
-              state={{patient, patientUuid: patient.id}}
-            />
-          </aside>
+          <div>
+            <aside>
+              <ExtensionSlot
+                extensionSlotName="patient-header-slot"
+                state={{patient, patientUuid: patient.id}}
+              />
+            </aside>
+          </div>
+          <br></br>
+          <br></br>
+          <Link
+            to={{
+              pathname: '/patient/' + patient.id + '/report',
+              state: {patient},
+            }}
+          >
+            <Button>Upload Report</Button>
+          </Link>
         </div>
       )}
     </main>
