ea4d6bf84ab86b0e1dc473b41b01706272239537
Abinaya U <abinaya.u@thoughtworks.com>
2022-04-21 15:50:41 +0530

    BAH-1544 | add. types for pending lab orders
    Co-authored-by: Arjun G <arjun.g@thoughtworks.com>

--- src/patient-lab-details/patient-lab-details.tsx
+++ src/patient-lab-details/patient-lab-details.tsx
@@ -1,25 +1,23 @@
-import {ExtensionSlot, usePatient} from '@openmrs/esm-framework'
+import AddFilled16 from '@carbon/icons-react/lib/add/16'
+import { ExtensionSlot, openmrsFetch, usePatient } from '@openmrs/esm-framework'
 import {
   Button,
-  DataTable,
-  InlineLoading,
-  Table,
+  DataTable, Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
   TableSelectAll,
-  TableSelectRow,
+  TableSelectRow
 } from 'carbon-components-react'
 import React from 'react'
-import {Link, RouteComponentProps} from 'react-router-dom'
+import { Link, RouteComponentProps } from 'react-router-dom'
 import useSWR from 'swr'
-import {labOrderUuid, headers} from '../constants'
+import { headers, labOrderUuid } from '../constants'
 import Loader from '../loader/loader.component'
+import { LabOrdersFetchResponse } from '../types'
 import styles from './patient-lab-details.scss'
-import {openmrsFetch} from '@openmrs/esm-framework'
-import AddFilled16 from '@carbon/icons-react/lib/add/16'
 
 interface PatientParamsType {
   patientUuid: string
@@ -35,59 +33,66 @@ const PatientLabDetails: React.FC<RouteComponentProps<PatientParamsType>> = ({
     openmrsFetch(url, {
       method: 'GET',
     })
-  const {data: pendingLabOrders, error: labOrderDataError, isValidating} = useSWR<
-    any,
+
+  const {data: pendingLabOrders, error: pendingLabOrderDataError} = useSWR<
+    LabOrdersFetchResponse,
     Error
   >(
     `/ws/rest/v1/order?patient=${patientUuid}&orderType=${labOrderUuid}&v=default`,
     fetcher,
   )
-  const rows = pendingLabOrders?.data?.results.map(row => {
+  const rows = pendingLabOrders?.data?.results?.map(row => {
     return {
       id: row.uuid,
       testName: row.display,
       date: new Date(row.dateActivated).toLocaleDateString(
         localStorage.getItem('i18nextLng'),
-        {year: 'numeric', month: 'long', day: '2-digit'},
+        {
+          year: 'numeric',
+          month: 'long',
+          day: '2-digit',
+        },
       ),
       orderedBy: row.orderer.display,
     }
   })
 
-  const renderPendingOrdersTable = () =>
-    isValidating ? (
-      <InlineLoading />
-    ) : labOrderDataError ? (
-      <div>Something went wrong: {error.message}</div>
-    ) : (
-      <>
-        <h4>Pending Lab Orders</h4>
-        <DataTable rows={rows} headers={headers}>
-          {({rows, headers, getSelectionProps}) => (
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableSelectAll {...getSelectionProps()} />
-                  {headers.map(header => (
-                    <TableHeader>{header.header}</TableHeader>
+  const renderPendingOrdersTable = () => (
+    <div>
+      <h4>Pending Lab Orders</h4>
+      <DataTable rows={rows} headers={headers}>
+        {({
+          rows,
+          headers,
+          getSelectionProps,
+          getHeaderProps,
+        }) => (
+          <Table title="lab-order-table">
+            <TableHead>
+              <TableRow>
+                <TableSelectAll {...getSelectionProps()} />
+                {headers.map(header => (
+                  <TableHeader {...getHeaderProps({header})}>
+                    {header.header}
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.id}>
+                  <TableSelectRow {...getSelectionProps({row})} />
+                  {row.cells.map(cell => (
+                    <TableCell key={cell.id}>{cell.value}</TableCell>
                   ))}
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map(row => (
-                  <TableRow key={row.id}>
-                    <TableSelectRow {...getSelectionProps({row})} />
-                    {row.cells.map(cell => (
-                      <TableCell key={cell.id}>{cell.value}</TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          )}
-        </DataTable>{' '}
-      </>
-    )
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </DataTable>
+    </div>
+  )
 
   return (
     <main className={styles.chartContainer}>
@@ -111,7 +116,11 @@ const PatientLabDetails: React.FC<RouteComponentProps<PatientParamsType>> = ({
           </div>
           <br></br>
           <br></br>
-          <div>{renderPendingOrdersTable()}</div>
+          {pendingLabOrderDataError ? (
+            <div>Something went wrong in fetching pending lab orders...</div>
+          ) : (
+            rows?.length > 0 && renderPendingOrdersTable()
+          )}
           <br></br>
           <br></br>
           <Link
