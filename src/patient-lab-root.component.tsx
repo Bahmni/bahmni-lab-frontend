540f194877bf26c01c635b786ae423d1675bc28e
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2022-07-07 17:27:49 +0530

    BAH 1645 | Users without privilege should not be able to access Lab Lite by directly hitting the endpoint URL (#24)

--- src/patient-lab-root.component.tsx
+++ src/patient-lab-root.component.tsx
@@ -1,28 +1,44 @@
 import React from 'react'
 import {SWRConfig} from 'swr'
 import {BrowserRouter, Route} from 'react-router-dom'
-import {patientLabDetailsRoute, spaRoot} from './constants'
+import {patientLabDetailsRoute, spaRoot, privilegeLabLite} from './constants'
 import PatientLabDetails from './patient-lab-details/patient-lab-details'
+import {UserHasAccess} from './UserHasAccess.component'
+import {InlineNotification} from 'carbon-components-react/lib/components/Notification'
 
 const swrConfiguration = {
   // Maximum number of retries when the backend returns an error
   errorRetryCount: 3,
   revalidateOnFocus: false,
 }
+const unauthorisedResponse = (
+  <div className="omrs-inline-notifications-container">
+    <InlineNotification
+      title="Unauthorised"
+      subtitle="You are unauthorised to view this page. Please contact your administrator"
+      kind="error"
+    />
+  </div>
+)
 
 const Root: React.FC = () => {
   return (
-    <main>
-      <SWRConfig value={swrConfiguration}>
-        <BrowserRouter basename={spaRoot}>
-          <Route
-            exact
-            path={patientLabDetailsRoute}
-            component={PatientLabDetails}
-          />
-        </BrowserRouter>
-      </SWRConfig>
-    </main>
+    <UserHasAccess
+      privilege={privilegeLabLite}
+      unauthorisedResponse={unauthorisedResponse}
+    >
+      <main>
+        <SWRConfig value={swrConfiguration}>
+          <BrowserRouter basename={spaRoot}>
+            <Route
+              exact
+              path={patientLabDetailsRoute}
+              component={PatientLabDetails}
+            />
+          </BrowserRouter>
+        </SWRConfig>
+      </main>
+    </UserHasAccess>
   )
 }
 
