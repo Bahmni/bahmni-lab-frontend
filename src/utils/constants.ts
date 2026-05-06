c6bafae129951519cde8f3ec4afda22e02a91c43
Aishwarya-thoughtworks <aishwarya.sontakke@thoughtworks.com>
2026-05-06 21:57:18 +0530

    BAH-4222-fix|Fir.PR review comments

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const spaRoot = window['getOpenmrsSpaBase']()
 export const labliteModuleName = '@bahmni/lab-lite-app'
 export const searchModuleName = '@openmrs/esm-patient-search-app'
+export const labAppNamespace = '@bahmni/lab-app'
 export const patientLabDetailsPath = 'patient/${patientUuid}'
 export const patientLabDetailsRoute = '/patient/:patientUuid'
 export const labOrderUuid = '8189b409-3f10-11e4-adec-0800271c1b75'
